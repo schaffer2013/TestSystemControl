4e14f85e94453a0392eed8d8173734dd9013316c
schaffer2013 <schaffer2013@gmail.com>
2015-06-04 23:14:48 -0400

    Initial Commit

---
+++ TestSystemControl.ino
@@ -0,0 +1,38 @@
+/*
+Full Control for Single Muscle
+*/
+
+#include <Servo.h>
+
+Servo inValveServo;
+Servo outValveServo;
+int inValvePin = 9;
+int outValvePin = 10;
+
+int inValvePos = 0;    // variable to store the servo position
+int outValvePos = 0;
+
+int pos=0;
+
+void setup()
+{
+  inValveServo.attach(inValvePin);  // attaches the servo on pin 9 to the servo object
+  outValveServo.attach(outValvePin);
+}
+
+void loop()
+{
+  for (pos = 0; pos <= 180; pos += 180) // goes from 0 degrees to 180 degrees
+  { // in steps of 1 degree
+    inValveServo.write(pos);
+    outValveServo.write(pos);     // tell servo to go to position in variable 'pos'
+    delay(500);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 180 ) // goes from 180 degrees to 0 degrees
+  {
+    inValveServo.write(pos);
+    outValveServo.write(pos);     // tell servo to go to position in variable 'pos'
+    delay(500);                       // waits 15ms for the servo to reach the position
+  }
+}
+
